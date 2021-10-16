c373e5b49b0948a7863b2401cb2408580e3c05dd
Brian Vraamark <bvr@sweetgeeks.dk>
2021-10-16 19:06:07 +0200

    Added Attach/detach debugger button

---
+++ EasyRun/AttachProperties/DebuggerAttachedProperties.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace EasyRun.AttachProperties
+{
+    public static class DebuggerAttachedProperties
+    {
+        public static readonly DependencyProperty DebuggerAttachedProperty
+                            = DependencyProperty.RegisterAttached("DebuggerAttached", typeof(bool), typeof(DebuggerAttachedProperties),
+                                new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.Inherits));
+
+        public static bool GetDebuggerAttached(DependencyObject element)
+            => (bool)element.GetValue(DebuggerAttachedProperty);
+
+        public static void SetDebuggerAttached(DependencyObject element, bool value)
+            => element.SetValue(DebuggerAttachedProperty, value);
+    }
+}
