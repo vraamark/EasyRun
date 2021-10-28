e9541d321b17a932a0c346d43438a6259f714edb
Brian Vraamark <bvr@sweetgeeks.dk>
2021-10-28 21:51:45 +0200

    Made compiler symbol to know what extension is running.

--- EasyRunShared/EasyRunToolWindow.cs
+++ EasyRunShared/EasyRunToolWindow.cs
@@ -23,7 +23,11 @@ namespace EasyRun
         /// </summary>
         public EasyRunToolWindow() : base(null)
         {
+#if EasyRun2019
             this.Caption = "EasyRun";
+#else
+            this.Caption = "Easy Run";
+#endif
             this.BitmapImageMoniker = CustomMonikers.EasyRun;
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
